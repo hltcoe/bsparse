8af266370089738ec9ff929fcbcf3a758a090c7e
Andrew Yates <andrew.yates@gmail.com>
2025-10-20 20:24:06 -0400

    import

---
+++ java/io/anserini/collection/ScaledJsonVectorCollection.java
@@ -0,0 +1,107 @@
+/*
+ * Modified JsonVectorCollection that modifies counts by a scaling factor of 50. Based on JsonVectorCollection.java from
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.collection;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * A JSON document collection where the user can specify directly the vector to be indexed.
+ */
+public class ScaledJsonVectorCollection extends DocumentCollection<ScaledJsonVectorCollection.Document> {
+  public ScaledJsonVectorCollection(Path path) {
+    this.path = path;
+  }
+
+  public ScaledJsonVectorCollection() {
+  }
+
+  @Override
+  public FileSegment<ScaledJsonVectorCollection.Document> createFileSegment(Path p) throws IOException {
+    return new ScaledJsonVectorCollection.Segment<>(p);
+  }
+
+  @Override
+  public FileSegment<ScaledJsonVectorCollection.Document> createFileSegment(BufferedReader bufferedReader) throws IOException {
+    return new ScaledJsonVectorCollection.Segment<>(bufferedReader);
+  }
+
+  public static class Segment<T extends ScaledJsonVectorCollection.Document> extends JsonCollection.Segment<T> {
+    public Segment(Path path) throws IOException {
+      super(path);
+    }
+
+    public Segment(BufferedReader bufferedReader) throws IOException {
+      super(bufferedReader);
+    }
+
+    @Override
+    protected Document createNewDocument(JsonNode json) {
+      return new Document(json);
+    }
+  }
+
+  public static class Document extends JsonCollection.Document {
+    private final String contents;
+    private final float[] vectorData;
+
+    public Document(JsonNode json) {
+      super(json);
+
+      // We're going to take the map associated with "vector" and generate pseudo-document.
+      JsonNode vectorNode = json.get("vector");
+
+      if (vectorNode.isArray()) {
+        // Dense vector format - store directly
+        vectorData = new float[vectorNode.size()];
+        for (int i = 0; i < vectorNode.size(); i++) {
+          vectorData[i] = (float) vectorNode.get(i).asDouble();
+        }
+        this.contents = null;
+      } else {
+        // Sparse vector format
+        // Generate pseudo-document by appending the feature cnt times
+        // where cnt is the value of the feature
+        final StringBuilder sb = new StringBuilder();
+        vectorNode.fields().forEachRemaining(e -> {
+          int cnt = (int) (50 * e.getValue().asDouble());
+          for (int i = 0; i < cnt; i++) {
+            sb.append(e.getKey()).append(" ");
+          }
+        });
+        this.contents = sb.toString();
+        vectorData = null; // No dense vector for sparse format
+      }
+
+    }
+
+    @Override
+    public String contents() {
+      return contents;
+    }
+
+    @Override
+    public float[] vector() {
+      return vectorData;
+    }
+  }
+}
+
